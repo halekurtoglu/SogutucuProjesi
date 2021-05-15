a373a692f876f9bead6a884593979bf7805c8c05
halekurtoglu <63074699+halekurtoglu@users.noreply.github.com>
2021-05-15 17:40:08 +0300

    Update Main.java

--- proje/SogutucuProjesi/src/sogutucuProjesi/Main.java
+++ proje/SogutucuProjesi/src/sogutucuProjesi/Main.java
@@ -1,6 +1,6 @@
 /**
 *
-* @author Halenur Kurtoðlu / halenur.kurtoglu@ogr.sakarya.edu.tr / g191210067
+* @author Halenur KurtoÃ°lu / halenur.kurtoglu@ogr.sakarya.edu.tr / g191210067
 * @since 15.05.2021
 * 
 **/
@@ -10,13 +10,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		AgArayuzu arayuz = new AgArayuzu();
-	    Eyleyici eyleyici= new Eyleyici();
-	    SicaklikAlgilayici algilayici=new SicaklikAlgilayici();
+	        Eyleyici eyleyici= new Eyleyici();
+	        SicaklikAlgilayici algilayici=new SicaklikAlgilayici();
 	     
 		Publisher p = new Publisher();
 		p.attach(arayuz);
-        p.attach(eyleyici);
-        p.attach(algilayici);
+                p.attach(eyleyici);
+                p.attach(algilayici);
 
 		AnaIslemPlatformu program=new AnaIslemPlatformu(p);
 		program.bildir("Cihaz acik");
